47ccccb241a9cd167163c296adf5d68cad7ad3b3
孙天宇 <2016011108@secoder.net>
2019-10-07 17:24:40 +0800

    unit-test

---
+++ tests.py
@@ -0,0 +1,28 @@
+'''
+Test suite for basic views
+'''
+import django
+from django.test import TestCase
+from django.test import Client
+import os
+
+os.environ.update({"DJANGO_SETTINGS_MODULE": "django_auth_example.settings"})
+django.setup()
+
+# class TestIndex(TestCase):
+#     '''Test case for "/"'''
+#
+#     def test_get(self):
+#         '''Test GET /'''
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_not_allowed(self):
+#         '''Test method not allowed'''
+#         response = self.client.post('/')
+#         self.assertEqual(response.status_code, 405)
+
+def test():
+    client = Client()
+    response = client.get('/')
+    assert response.status_code == 200
