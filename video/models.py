8acbfa063f6fc85da7848fe33e9d24300ac1d455
2016011108 <2016011108@secoder.net>
2019-10-24 23:34:25 +0800

    fix route

--- video/models.py
+++ video/models.py
@@ -1,10 +1,12 @@
-
+import os
 from django.db import models
 # Create your models here.
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+VIDEO_DIR = os.path.join(BASE_DIR, 'uploads')
 class Video(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
     description = models.CharField(max_length=255, blank=True, null=True)
-    file = models.FileField(upload_to='video/uploads')
+    file = models.FileField(upload_to=VIDEO_DIR)
     create_time = models.DateTimeField(auto_now_add=True, blank=True, max_length=20)
 
     class Meta:
