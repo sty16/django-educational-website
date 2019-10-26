eafdae1208b1ba38593e96ea2ead3f22ece9ff94
2016011108 <2016011108@secoder.net>
2019-10-26 13:40:11 +0800

    change

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -141,21 +141,21 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 #database 选择db.sqlite3或者mysql
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'satellite',
-#         'USER': 'root',
-#         'PASSWORD': '123456',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',  # 端口
-#         "OPTIONS": {"init_command": "SET default_storage_engine=INNODB;"}
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'satellite',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',  # 端口
+        "OPTIONS": {"init_command": "SET default_storage_engine=INNODB;"}
+    }
+    }
