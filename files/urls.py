9d68f300d0a52bd5ae80cc0c21c4b31d446bcac2
2016011130 <2016011130@secoder.net>
2019-10-29 17:49:25 +0800

    changed html base

--- files/urls.py
+++ files/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.urls import path
 
-app_name = 'file'
+app_name = 'files'
 urlpatterns = [
     path('',views.index, name='index'),
     path('<int:file_id>/',views.detail, name='detail'),
