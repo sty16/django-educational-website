ff8db9af9c3220f886e5ccd322487b4aecb4f964
2016011130 <2016011130@secoder.net>
2019-10-31 01:00:35 +0800

    connect to fileshow

--- files/urls.py
+++ files/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 app_name = 'files'
 urlpatterns = [
     path('',views.index, name='index'),
-    path('<int:file_id>/',views.detail, name='detail'),
-    path('file_show/',views.file_show.as_view(), name='file_show'),
+    path('file_show/<str:user_name>/<str:file_name>',views.file_show.as_view(), name='file_show'),
     path('upload/',views.upload, name='file_upload'),
 ]
