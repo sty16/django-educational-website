2b00d311b45d493fc8eca14955fefc2a81423441
2016011108 <2016011108@secoder.net>
2019-11-01 22:56:19 +0800

    code download

--- static/js/code_upload.js
+++ static/js/code_upload.js
@@ -17,7 +17,78 @@ $(function(){
             complete: function(XMLHttpRequest){
                 $('#CodeUploadBtn').val("上传文件");
                 $('#CodeUploadBtn').removeAttr("disabled");
+                alert('文件上传成功')
+            },
+            success: function(result) {
+                $('html').html(result)
             }
         });
     })
+    $('.getcode').on('click', function(){
+        var obj = $(this);
+        var id = obj.attr("id");
+        var formdata = new FormData();
+        var csrfToken = $("[name='csrfmiddlewaretoken']").val();
+        formdata.append("File_id", id);
+        $.ajax({
+            cache:false,
+            type:"post",
+            url:"/coding/download/",
+            async:true,
+            data:{'File_id':id, "csrfmiddlewaretoken": csrfToken},
+            beforeSend:function(XMLHttpRequest){
+                obj.val("下载中...");
+                obj.attr('disabled',true);
+            },
+            success:function(data){
+                if (data.status =="success"){
+                    // location.reload()
+                    // sleep(1000);
+                    location.href = '/media/' + data.FilePath;
+                    // $.ajax({
+                    //     cache:false,
+                    //     type:"post",
+                    //     url:"/coding/download/",
+                    //     async:true,
+                    // });
+                }else{
+                    // window.location.href = '/media/' + data.FilePath;
+                    alert("对不起，网络错误，请稍后重试或联系管理员")
+                }
+            },
+            complete: function(XMLHttpRequest){
+                obj.val("下载代码");
+                obj.removeAttr("disabled");
+            }
+    });
+    })
 });
+// function download_file(btn){
+//     var obj = $(btn);
+//     var id = obj.attr("id");
+//     var formdata = new FormData();
+//     formdata.append("File_id", id);
+//     alert(id)
+//     $.ajax({
+//         cache:false,
+//         type:"post",
+//         url:"coding/download/",
+//         async:true,
+//         data:formdata,
+//         beforeSend:function(XMLHttpRequest){
+//             obj.val("下载中...");
+//             obj.attr('disabled',true);
+//         },
+//         complete: function(XMLHttpRequest){
+//             obj.val("下载文件");
+//             obj.removeAttr("disabled");
+//         },
+//         success:function(data){
+//             if (data.status == True){
+//                 window.location.href = data.FilePath;
+//             }else{
+//                 alert("对不起，网络错误，请稍后重试或联系管理员")
+//             }
+//         }
+//     });
+//     }
