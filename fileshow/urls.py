11e64a647dc87099cda71f4c002ca5efed2318f4
2016011130 <2016011130@secoder.net>
2019-11-04 01:30:37 +0800

    fixed some bugs

--- fileshow/urls.py
+++ fileshow/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
-
 app_name = 'fileshow'
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('coding/<str:user_name>/<str:file_name>',views.file_show.as_view(), name='file_show'),
-    path('upload/',views.upload, name='file_upload'),
+    path('coding/<str:user_name>/<str:file_name>',views.file_show, name='file_show'),
 ]
