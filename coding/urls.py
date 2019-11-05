b8aac8a05888f4e21646c597d252d50b5be12084
吕凯 <2016011138@secoder.net>
2019-11-05 13:45:58 +0000

    add sort urls

--- coding/urls.py
+++ coding/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'list/',CodeListView.as_view(),name='code_list'),
     url(r'upload/',CodeUploadView.as_view(), name='upload'),
     url(r'download/', CodeDownloadView.as_view(),name='download'),
-    url(r'sort_bytime',CodeListByTimeView.as_view(),name="sort_bytime")
+    url(r'sort_bytime',CodeListByTimeView.as_view(),name="sort_bytime"),
+    url(r'sort_bydownload',CodeListByDownloadView.as_view(),name="sort_bydownload"),
+    url(r'sort_bylikes',CodeListByLikesView.as_view(),name="sort_bylikes"),
 ]
