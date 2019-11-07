08add4374339a0fb097e3ad4743b6fb72b91f825
2016011108 <2016011108@secoder.net>
2019-11-07 19:45:04 +0800

    change settings to save file

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -148,7 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'media', 'db.sqlite3'),
     }
 }
 
