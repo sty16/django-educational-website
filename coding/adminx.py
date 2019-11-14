98d2b1c7214ee0fc6508f95a5aaced6028ee7920
2016011108 <2016011108@secoder.net>
2019-11-14 23:29:31 +0800

    add admin

--- coding/adminx.py
+++ coding/adminx.py
@@ -6,9 +6,9 @@ from .models import Code, LikeRecord
 class CodeAdmin(object):
     '''课程'''
 
-    list_display = [ 'codename','desc','userinfo','fav_nums','download_nums','add_time','syntax_check','manual_check','codefile']   #显示的字段
-    search_fields =[ 'codename','desc','userinfo','fav_nums','download_nums','add_time','syntax_check','manual_check','codefile']  #搜索          #搜索
-    list_filter =[ 'codename','desc','userinfo','fav_nums','download_nums','add_time','syntax_check','manual_check','codefile']   #过滤
+    list_display = [ 'codename','desc','userinfo','fav_nums','download_nums','add_time','syntax_check','manual_check','result_check','star_check', 'codefile']   #显示的字段
+    search_fields =[ 'codename','desc','userinfo','fav_nums','download_nums','add_time','syntax_check','manual_check','result_check','star_check','codefile']  #搜索          #搜索
+    list_filter =[ 'codename','desc','userinfo','fav_nums','download_nums','add_time','syntax_check','result_check','star_check','manual_check','codefile']   #过滤
 
 # 将管理器与model进行注册关联
 class LikeAdmin(object):
