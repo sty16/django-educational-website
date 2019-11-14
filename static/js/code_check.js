0d8e53af0929c362f5fa0c2b368f950516e5ea61
2016011108 <2016011108@secoder.net>
2019-11-14 23:20:08 +0800

    add check_frontend

--- static/js/code_check.js
+++ static/js/code_check.js
@@ -58,4 +58,40 @@ function QueryFilename(file_id){
     });
     return filename;
 }
+    $('.codecheck').on('click',function(){
+        var code_obj = $(this);
+        var code_id = String(code_obj.attr('name'));
+        console.log(code_id);
+        var check_result = $("input[name=" + code_id + "]:checked").val();
+        var csrfToken = $("[name='csrfmiddlewaretoken']").val();
+        console.log(check_result);
+        if (check_result != undefined){
+            send = confirm('确认提交结果');
+        }else{
+            alert('您还没有选择审核结果');
+        }
+        if(send){
+            $.ajax({
+            cache:false,
+            type:"post",
+            url:"/coding/code_check/",
+            async:true,
+            data:{'File_id':code_id, "csrfmiddlewaretoken": csrfToken,"result":check_result},
+            beforeSend:function(XMLHttpRequest){
+                code_obj.attr('disabled',true);
+            },
+            success:function(data){
+                if (data){
+                   alert('提交成功')
+                   location.reload(); 
+                }else{
+                    alert("对不起，网络错误，请稍后重试或联系管理员")
+                }
+            },
+            complete: function(XMLHttpRequest){
+                code_obj.removeAttr("disabled");
+            }
+            });
+        }
+    })
 })
