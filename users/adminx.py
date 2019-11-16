55409e237d2ee4e7ec0adfbb16478c52e03b9bf3
2016011108 <2016011108@secoder.net>
2019-11-16 17:01:58 +0800

    add is_admin interface

--- users/adminx.py
+++ users/adminx.py
@@ -2,9 +2,9 @@ import xadmin
 from .models import User
 
 class UserAdmin(object):
-    list_display = ['username', 'email', 'date_joined','nickname','is_active','mobile','image']
-    search_fields = ['username', 'email', 'date_joined']
-    list_filter = ['username', 'email', 'date_joined']
+    list_display = ['username', 'email', 'date_joined','nickname','is_admin','is_active','mobile','image']
+    search_fields = ['username', 'email', 'date_joined','mobile']
+    list_filter = ['username', 'email', 'date_joined','mobile']
 
 # admin.site.unregister(User)
 xadmin.site.unregister(User)
