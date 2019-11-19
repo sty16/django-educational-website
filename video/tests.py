4e3651131cdb9971e24571181ae7225421411211
2016011196 <2016011196@secoder.net>
2019-11-20 03:11:48 +0800

    modified test.py

--- video/tests.py
+++ video/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from video.models import Video
 
 # Create your tests here.
+class VideoTestCase(TestCase):
+    def setUp(self):
+        #Video.objects.create(title="Alice",description="Alice's Video",file='alice.mp4')
+    
+    def test_video_uploaded(self):
+        #test_video=Video.objects.get(name='Alice')
+        self.assertEqual('Alice','Alice')
+
+
+    #writing somethins 
+    #to fully test all database
