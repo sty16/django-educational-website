e665d0970378ace8fcdc3ac92bb66a9803d4f3b1
2016011196 <2016011196@secoder.net>
2019-11-20 21:04:23 +0800

    finished video test.py

--- video/tests.py
+++ video/tests.py
@@ -1,15 +1,36 @@
 from django.test import TestCase
 from video.models import Video
 
-# Create your tests here.
-class VideoTestCase(TestCase):
-    def setUp(self):
-        #Video.objects.create(title="Alice",description="Alice's Video",file='alice.mp4')
-    
-    def test_video_uploaded(self):
-        #test_video=Video.objects.get(name='Alice')
-        self.assertEqual('Alice','Alice')
+class VideoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Video.objects.create(title='alice',description="alice's video")
+
+    def test_title_label(self):
+        name=Video.objects.get(id=1)
+        field_label=name._meta.get_field('title').verbose_name
+        self.assertEquals(field_label,'title')
+        
+    def test_description_label(self):
+        name=Video.objects.get(id=1)
+        field_label=name._meta.get_field('description').verbose_name
+        self.assertEquals(field_label,'description')
+
+    def test_file_label(self):
+        name=Video.objects.get(id=1)
+        field_label=name._meta.get_field('file').verbose_name
+        self.assertEquals(field_label,'file')
+
+    def test_create_time_label(self):
+        name=Video.objects.get(id=1)
+        field_label=name._meta.get_field('create_time').verbose_name
+        self.assertEquals(field_label,'create time')
+
+    def test_title_max_length(self):
+        name=Video.objects.get(id=1)
+        max_length=name._meta.get_field('title').max_length
+        self.assertEquals(max_length,100)
 
 
-    #writing somethins 
-    #to fully test all database
